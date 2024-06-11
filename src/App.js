a3e97bbc625150a04567f1c93919f2261b55ccba
delroscol98 <collindelrosario98@gmail.com>
2024-06-11 22:12:55 +1000

    CSS modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="app">test</div>;
+  return (
+    <div className="app">
+      <div className="calculator">CALCULATOR</div>
+      <div className="divider">DIVIDER</div>
+      <div className="results">RESULTS</div>
+    </div>
+  );
 }
 
 export default App;
